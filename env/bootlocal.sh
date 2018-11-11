29052f3e9fb7e4449bfef88b1619173de1b7e920
myzero1 <myzero1@sina.com>
2018-11-11 15:12:15 +0800

    holdon

--- env/bootlocal.sh
+++ env/bootlocal.sh
@@ -2,5 +2,6 @@
 sudo sed -i "s|EXTRA_ARGS='|EXTRA_ARGS='--registry-mirror=http://f1361db2.m.daocloud.io |g" /var/lib/boot2docker/profile
 
 #copy env files
+mkdir -p /var/docker-worspace/docker-app/data;
 mkdir -p /var/docker-worspace/docker-app;
 cp -R /c/Users/woogle/docker-workspace/docker-app/env  /var/docker-worspace/docker-app/env;

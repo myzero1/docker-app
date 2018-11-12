e8d2098388a50f1916fa44e62a4a7d6fcf856c4a
myzero1 <myzero1@sina.com>
2018-11-12 16:14:50 +0800

    holdon

--- env/bootlocal.sh
+++ env/bootlocal.sh
@@ -3,5 +3,7 @@ sudo sed -i "s|EXTRA_ARGS='|EXTRA_ARGS='--registry-mirror=http://f1361db2.m.daoc
 
 #copy env files
 sudo mkdir -p /var/docker-worspace/docker-app/data;
-sudo mkdir -p /var/docker-worspace/docker-app;
-sudo cp -Rf /c/Users/woogle/docker-workspace/docker-app/env  /var/docker-worspace/docker-app/env;
+sudo mkdir -p /var/docker-worspace/docker-app/env;
+#sudo cp -Rf /c/Users/docker-app/env  /var/docker-worspace/docker-app/env;
+# notice! change the username 'Administrator'.
+sudo cp -Rf /c/Users/Administrator/.docker/docker-workspace/docker-app/env  /var/docker-worspace/docker-app/env;

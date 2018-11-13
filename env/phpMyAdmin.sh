09e5952f47d598027e43993e36e4d965793278a7
myzero1 <myzero1@sina.com>
2018-11-13 13:54:48 +0800

    clear

--- env/phpMyAdmin.sh
+++
@@ -1,9 +0,0 @@
-ver='4.7.7';
-cd /www/web;
-wget https://files.phpmyadmin.net/phpMyAdmin/$ver/phpMyAdmin-$ver-all-languages.zip;
-unzip phpMyAdmin-$ver-all-languages.zip;
-mv phpMyAdmin-$ver-all-languages  phpmyadmin;
-cd phpmyadmin;
-sudo sed -i "s/^\$cfg\['Servers'\]\[\$i\]\['host'\].*$/\$cfg['Servers'][\$i]['host'] = 'mysql';/g" libraries/config.default.php;
-sudo sed -i "s/^\$cfg\['Servers'\]\[\$i\]\['password'\].*$/\$cfg['Servers'][\$i]['password'] = 'myzero1&735';/g" libraries/config.default.php;
-# rm phpMyAdmin-$ver-all-languages.zip;
